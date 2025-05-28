24a142a9b62c3e821f11294ae430cdbff493fbbb
Sara Beatriz Bento <167379586+SaraBeatris@users.noreply.github.com>
2025-05-27 22:33:47 -0300

    Update settings.py

--- ProjetoPI/ProjetoPI/settings.py
+++ ProjetoPI/ProjetoPI/settings.py
@@ -129,6 +129,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
