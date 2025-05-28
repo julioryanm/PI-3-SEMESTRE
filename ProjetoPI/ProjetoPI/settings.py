d2dc6905bd122d54e3ff5e086f590c1cc9339051
Sara Beatriz Bento <167379586+SaraBeatris@users.noreply.github.com>
2025-05-27 22:34:43 -0300

    Update settings.py

--- ProjetoPI/ProjetoPI/settings.py
+++ ProjetoPI/ProjetoPI/settings.py
@@ -36,13 +36,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'Sistema',
+    'Sistema',
+    'rest_framework',
+    'rest_framework.authtoken',
      
 ]
 MEDIA_URL = '/media/'
