1e9be279d047f5d42838164831638a219af8ff83
julioryanm <160801596+julioryanm@users.noreply.github.com>
2025-05-28 17:45:05 -0300

    Update forms.py
    hotel adicionado

--- ProjetoPI/Sistema/forms.py
+++ ProjetoPI/Sistema/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.forms import User
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from .models import Restaurante, Colaborador, Profile
+from .models import Restaurante, Colaborador, Profile, Hotel
  
 # Cadatro restaurantes
 # class CadastroRestauranteForm(Restaurante):
@@ -15,6 +15,10 @@ class CadastroRestauranteForm(forms.ModelForm):
         model = Restaurante
         fields = ['nome', 'cnpj', 'endereco', 'telefone', 'responsavel']
  
+class CadastroHotelForm(forms.ModelForm):
+    class Meta:
+        model = Hotel
+        fields = ['nome', 'cnpj', 'cidade', 'endereco', 'telefone', 'responsavel']
  
 # Cadatro usuarios
 class CustomUserCreationForm(UserCreationForm):
