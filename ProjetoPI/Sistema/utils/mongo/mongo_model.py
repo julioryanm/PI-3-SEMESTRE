fe1c5647512fd9b75b17391d7fe4fad6da1e92d1
João Carossine <133826422+joaopcarossine@users.noreply.github.com>
2025-06-16 21:44:58 -0300

    update para controle refeições

--- ProjetoPI/Sistema/utils/mongo/mongo_model.py
+++ ProjetoPI/Sistema/utils/mongo/mongo_model.py
@@ -1,29 +1,34 @@
 from bson import ObjectId
 from Sistema.utils.mongo.mongo_connection import get_mongo_client
+from datetime import datetime
+from Sistema.models import Colaborador
 
-class Pedido:
+class ControleRefeicoes:
     def __init__(self):
         self.db = get_mongo_client()
-        self.collection = self.db["pedidos"]
+        self.collection = self.db["controle_diario"]  # nova coleção
 
-    def criar_pedido(self, tipo, valor):
-        pedido = {"tipo": tipo, "valor": float(valor)}
-        return self.collection.insert_one(pedido)
+    def registrar_refeicoes(self, data, colaboradores_ids, usuario):
+        registros = []
+        data_formatada = datetime.strptime(data, "%Y-%m-%d")
 
-    def listar_pedidos(self):
-        pedidos = list(self.collection.find())
-        for pedido in pedidos:
-            pedido["id"] = str(pedido["_id"])
-        return pedidos
+        for colaborador_id in colaboradores_ids:
+            try:
+                colaborador = Colaborador.objects.get(id=colaborador_id)
+                registro = {
+                    "colaborador_id": colaborador.id,
+                    "colaborador_nome": colaborador.nome,
+                    "obra_id": colaborador.obra.id,
+                    "obra_nome": colaborador.obra.nome,
+                    "data_refeicao": data_formatada,
+                    "valor_refeicao": "8,00",
+                    "registrado_em": datetime.now(),
+                    "registrado_por_id": usuario.id,
+                    "registrado_por_nome": usuario.username,
+                }
+                registros.append(registro)
+            except Colaborador.DoesNotExist:
+                continue  # ignora se colaborador não encontrado
 
-    def buscar_pedido(self, pedido_id):
-        return self.collection.find_one({"_id": ObjectId(pedido_id)})
-
-    def atualizar_pedido(self, pedido_id, tipo, valor):
-        return self.collection.update_one(
-            {"_id": ObjectId(pedido_id)},
-            {"$set": {"tipo": tipo, "valor": float(valor)}}
-        )
-
-    def excluir_pedido(self, pedido_id):
-        return self.collection.delete_one({"_id": ObjectId(pedido_id)})
+        if registros:
+            self.collection.insert_many(registros)
